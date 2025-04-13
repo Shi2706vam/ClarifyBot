f92b96f2f949d04b0611b15846f641fa713966d5
Shi2706vam <shivamkumarroy223344@gmail.com>
2025-04-13 20:49:50 +0530

    bot

--- frontend/src/pages/Bot.jsx
+++ frontend/src/pages/Bot.jsx
@@ -1,52 +1,65 @@
-import { useMemo } from 'react';
-import { Container, Header, MessageList, Composer, ComposerInput, ComposerButton, WebchatProvider, getClient } from "@botpress/webchat";
-import { buildTheme } from "@botpress/webchat-generator";
+// import { useMemo } from 'react';
+// import { Container, Header, MessageList, Composer, ComposerInput, ComposerButton, WebchatProvider, getClient } from "@botpress/webchat";
+// import { buildTheme } from "@botpress/webchat-generator";
 
-const { theme, style } = buildTheme({
-  themeName: "prism",
-  themeColor: "#634433",
-});
+// const { theme, style } = buildTheme({
+//   themeName: "prism",
+//   themeColor: "#634433",
+// });
 
-const clientId = import.meta.env.CLIENT_ID; // Replace with your actual client ID
+// const clientId = import.meta.env.CLIENT_ID;
 
-const config = {
-  composerPlaceholder: "What would you like to know?",
-  botName: "Clarify",
-  botDescription:
-    "\"Clarify - Where question meets clarity\" , is an advanced AI agent designed to resolve your placement-related queries with precision and ease.",
-  email: {},
-  phone: {},
-  website: {},
-  termsOfService: {},
-  privacyPolicy: {},
-  color: "#3B82F6",
-  variant: "solid",
-  themeMode: "light",
-  fontFamily: "inter",
-  radius: 1
+// const config = {
+//   composerPlaceholder: "What would you like to know?",
+//   botName: "Clarify",
+//   botDescription:
+//     "\"Clarify - Where question meets clarity\" , is an advanced AI agent designed to resolve your placement-related queries with precision and ease.",
+//   email: {},
+//   phone: {},
+//   website: {},
+//   termsOfService: {},
+//   privacyPolicy: {},
+//   color: "#3B82F6",
+//   variant: "solid",
+//   themeMode: "light",
+//   fontFamily: "inter",
+//   radius: 1
+// };
+
+// export default function Bot() {
+//   const client = useMemo(() => getClient({ clientId }), []);
+
+//   return (
+//       <div className='w-[1024px] h-[95dvh] px-15 pb-5 pt-15'>
+//         <style>{style}</style>
+//         <WebchatProvider
+//           key={JSON.stringify(config)}
+//           theme={theme}
+//           configuration={config}
+//           client={client}
+//         >
+//           <Container>
+//             <Header />
+//             <MessageList />
+//             <Composer>
+//               <ComposerInput />
+//               <ComposerButton />
+//             </Composer>
+//           </Container>
+//         </WebchatProvider>
+//       </div>
+//   );
+// }
+
+import React from 'react';
+
+const Bot = () => {
+
+  return(
+    <div>
+      <iframe className='w-[1024px] h-[95dvh] px-15 pb-5 pt-15' src="https://shorturl.at/EkfnY"/>
+    </div>
+  )
 };
 
-export default function Bot() {
-  const client = useMemo(() => getClient({ clientId }), []);
-
-  return (
-      <div className='w-[1024px] h-[95dvh] px-15 pb-5 pt-15'>
-        <style>{style}</style>
-        <WebchatProvider
-          key={JSON.stringify(config)}
-          theme={theme}
-          configuration={config}
-          client={client}
-        >
-          <Container>
-            <Header />
-            <MessageList />
-            <Composer>
-              <ComposerInput />
-              <ComposerButton />
-            </Composer>
-          </Container>
-        </WebchatProvider>
-      </div>
-  );
-}
+export default Bot;
